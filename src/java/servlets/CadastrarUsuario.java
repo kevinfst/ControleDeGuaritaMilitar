96aee7874ea306a52449012b51ed4e4779f9805c
Kevin Santos <kevinst2610@gmail.com>
2023-11-23 19:30:24 -0300

    Implementação do Login/Cadastro
    Atualização do código com Login/Cadastro em funcionamento, verificação de dados etc.
    
    Co-Authored-By: Gustavo <60950245+GustavoSanchesPavani@users.noreply.github.com>
    Co-Authored-By: Leandro Berto <142258252+leandrobt11@users.noreply.github.com>

--- src/java/servlets/CadastrarUsuario.java
+++ src/java/servlets/CadastrarUsuario.java
@@ -3,6 +3,7 @@ package servlets;
 import br.com.DAO.usuarioDAO;
 import br.com.DTO.usuarioDTO;
 import java.io.IOException;
+import static java.lang.System.out;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,10 +17,22 @@ public class CadastrarUsuario extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        usuarioDTO objUsuarioDTO = new usuarioDTO();
-        objUsuarioDTO.setNm_usuario(request.getParameter("nm_usuario"));
-        usuarioDAO objUsuarioDAO = new usuarioDAO();
-        objUsuarioDAO.CadastrarUsuario(objUsuarioDTO);
+        try {
+            usuarioDTO objUsuarioDTO = new usuarioDTO();
+            objUsuarioDTO.setNm_usuario(request.getParameter("nm_usuario"));
+            objUsuarioDTO.setCd_idade(Integer.parseInt(request.getParameter("cd_idade")));
+            objUsuarioDTO.setDt_dataNascimento(request.getParameter("dt_dataNascimento"));
+            objUsuarioDTO.setNm_guerra(request.getParameter("nm_guerra"));
+            objUsuarioDTO.setNm_patente(request.getParameter("nm_patente"));
+            objUsuarioDTO.setNm_usuarioLogin(request.getParameter("nm_usuarioLogin"));
+            objUsuarioDTO.setCd_senha(request.getParameter("cd_senha"));
+
+            usuarioDAO objUsuarioDAO = new usuarioDAO();
+            objUsuarioDAO.CadastrarUsuario(objUsuarioDTO);
+            out.print(request.getParameter("nm_usuario"));
+        } catch (NumberFormatException e) {
+
+        }
 
     }
 
--- web/WEB-INF/backupSQL/soldiersSQL.sql
+++ web/WEB-INF/backupSQL/soldiersSQL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2023 at 08:38 AM
+-- Generation Time: Nov 23, 2023 at 08:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `soldiers`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `escalasguarda`
+--
+
+CREATE TABLE `escalasguarda` (
+  `id_escala` int(11) NOT NULL,
+  `dt_diaSemana` date DEFAULT NULL,
+  `dt_horarioInicio` time DEFAULT NULL,
+  `dt_horarioFim` time DEFAULT NULL,
+  `nm_patenteAutorizacao` varchar(20) DEFAULT NULL CHECK (`nm_patenteAutorizacao` in ('Sentinela','Comandante')),
+  `cd_autorizadoEscalaGuarda` tinyint(1) DEFAULT 0,
+  `cd_patenteUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trocashorario`
+--
+
+CREATE TABLE `trocashorario` (
+  `id_troca` int(11) NOT NULL,
+  `cd_soldadoSolicitante` int(11) DEFAULT NULL,
+  `cd_soldadoAceitante` int(11) DEFAULT NULL,
+  `dt_novoHorarioInicio` time DEFAULT NULL,
+  `dt_novoHorarioFim` time DEFAULT NULL,
+  `cd_autorizadoTrocasHorario` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,6 +82,21 @@ INSERT INTO `usuarios` (`id_usuario`, `nm_usuario`, `cd_idade`, `dt_dataNascimen
 --
 
 --
+-- Indexes for table `escalasguarda`
+--
+ALTER TABLE `escalasguarda`
+  ADD PRIMARY KEY (`id_escala`),
+  ADD KEY `cd_patenteUsuario` (`cd_patenteUsuario`);
+
+--
+-- Indexes for table `trocashorario`
+--
+ALTER TABLE `trocashorario`
+  ADD PRIMARY KEY (`id_troca`),
+  ADD KEY `cd_soldadoSolicitante` (`cd_soldadoSolicitante`),
+  ADD KEY `cd_soldadoAceitante` (`cd_soldadoAceitante`);
+
+--
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -62,10 +108,39 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `escalasguarda`
+--
+ALTER TABLE `escalasguarda`
+  MODIFY `id_escala` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trocashorario`
+--
+ALTER TABLE `trocashorario`
+  MODIFY `id_troca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `escalasguarda`
+--
+ALTER TABLE `escalasguarda`
+  ADD CONSTRAINT `escalasguarda_ibfk_1` FOREIGN KEY (`cd_patenteUsuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Constraints for table `trocashorario`
+--
+ALTER TABLE `trocashorario`
+  ADD CONSTRAINT `trocashorario_ibfk_1` FOREIGN KEY (`cd_soldadoSolicitante`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `trocashorario_ibfk_2` FOREIGN KEY (`cd_soldadoAceitante`) REFERENCES `usuarios` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
