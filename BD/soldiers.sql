adb445e4ac20dd472fbbc37d93534b2064fd062d
Kevin Santos <kevinst2610@gmail.com>
2024-05-25 01:43:09 -0300

    Inserção do novo Banco de Dados
    Co-Authored-By: Leandro Berto <leandrobertto1106@gmail.com>
    Co-Authored-By: Raynara Nascimento <90811047+raynaranasc@users.noreply.github.com>
    Co-Authored-By: Gustavo <60950245+GustavoSanchesPavani@users.noreply.github.com>

--- BD/soldiers.sql
+++ BD/soldiers.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 03:03 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 25/05/2024 às 06:42
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `soldiers`
+-- Banco de dados: `soldiers`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `escala_guarda`
+-- Estrutura para tabela `escala_guarda`
 --
 
 CREATE TABLE `escala_guarda` (
@@ -38,7 +38,24 @@ CREATE TABLE `escala_guarda` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registro_entrada_saida`
+-- Estrutura para tabela `historico_escala_guarda`
+--
+
+CREATE TABLE `historico_escala_guarda` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `data_hora_entrada` datetime DEFAULT NULL,
+  `data_hora_saida` datetime DEFAULT NULL,
+  `tipo_escala` varchar(50) DEFAULT NULL,
+  `corte_cabelo_conformidade` tinyint(1) DEFAULT NULL,
+  `identificacao_militar_conformidade` tinyint(1) DEFAULT NULL,
+  `data_remocao` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `registro_entrada_saida`
 --
 
 CREATE TABLE `registro_entrada_saida` (
@@ -51,21 +68,21 @@ CREATE TABLE `registro_entrada_saida` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `troca_horario`
+-- Estrutura para tabela `solicitar_observacao`
 --
 
-CREATE TABLE `troca_horario` (
-  `id_troca` int(11) NOT NULL,
+CREATE TABLE `solicitar_observacao` (
+  `id_observacao` int(11) NOT NULL,
   `id_usuario_solicitante` int(11) DEFAULT NULL,
-  `id_usuario_destinatario` int(11) DEFAULT NULL,
   `data_solicitacao` datetime DEFAULT NULL,
-  `status_solicitacao` varchar(20) DEFAULT NULL
+  `status_solicitacao` varchar(20) DEFAULT NULL,
+  `texto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura para tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -80,87 +97,91 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `escala_guarda`
+-- Índices de tabela `escala_guarda`
 --
 ALTER TABLE `escala_guarda`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indexes for table `registro_entrada_saida`
+-- Índices de tabela `historico_escala_guarda`
+--
+ALTER TABLE `historico_escala_guarda`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `registro_entrada_saida`
 --
 ALTER TABLE `registro_entrada_saida`
   ADD PRIMARY KEY (`id_registro`),
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indexes for table `troca_horario`
+-- Índices de tabela `solicitar_observacao`
 --
-ALTER TABLE `troca_horario`
-  ADD PRIMARY KEY (`id_troca`),
-  ADD KEY `id_usuario_solicitante` (`id_usuario_solicitante`),
-  ADD KEY `id_usuario_destinatario` (`id_usuario_destinatario`);
+ALTER TABLE `solicitar_observacao`
+  ADD PRIMARY KEY (`id_observacao`),
+  ADD KEY `id_usuario_solicitante` (`id_usuario_solicitante`);
 
 --
--- Indexes for table `usuario`
+-- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `escala_guarda`
+-- AUTO_INCREMENT de tabela `escala_guarda`
 --
 ALTER TABLE `escala_guarda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
 
 --
--- AUTO_INCREMENT for table `registro_entrada_saida`
+-- AUTO_INCREMENT de tabela `historico_escala_guarda`
+--
+ALTER TABLE `historico_escala_guarda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT de tabela `registro_entrada_saida`
 --
 ALTER TABLE `registro_entrada_saida`
-  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
--- AUTO_INCREMENT for table `troca_horario`
+-- AUTO_INCREMENT de tabela `solicitar_observacao`
 --
-ALTER TABLE `troca_horario`
-  MODIFY `id_troca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+ALTER TABLE `solicitar_observacao`
+  MODIFY `id_observacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
 
 --
--- AUTO_INCREMENT for table `usuario`
+-- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
--- Constraints for dumped tables
+-- Restrições para tabelas despejadas
 --
 
 --
--- Constraints for table `escala_guarda`
+-- Restrições para tabelas `escala_guarda`
 --
 ALTER TABLE `escala_guarda`
   ADD CONSTRAINT `escala_guarda_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
 --
--- Constraints for table `registro_entrada_saida`
+-- Restrições para tabelas `registro_entrada_saida`
 --
 ALTER TABLE `registro_entrada_saida`
   ADD CONSTRAINT `registro_entrada_saida_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
-
---
--- Constraints for table `troca_horario`
---
-ALTER TABLE `troca_horario`
-  ADD CONSTRAINT `troca_horario_ibfk_1` FOREIGN KEY (`id_usuario_solicitante`) REFERENCES `usuario` (`id_usuario`),
-  ADD CONSTRAINT `troca_horario_ibfk_2` FOREIGN KEY (`id_usuario_destinatario`) REFERENCES `usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
